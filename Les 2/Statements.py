f35628023e488119d703abb94f4109d3388e80da
Nikiforos2000 <nikiforos.athanasiou@student.hu.nl>
2018-09-18 19:02:50 +0200

    Huiswerk

---
+++ Les 2/Statements.py
@@ -0,0 +1,9 @@
+a = 6
+b = 7
+c = a+b/2
+voornaam = 'Nikiforos'
+achternaam = 'Athanasiou'
+mijnnaam = voornaam + ' ' + achternaam
+print(mijnnaam)
+print(b > 72 < a)
+print((len(voornaam + achternaam)) == (len(mijnnaam)))
---
+++ Les 2/Strings.py
@@ -0,0 +1,9 @@
+print(len('supercalifragilisticexpialidocious'))
+s = 'supercalifragilisticexpialidocious'
+i = 'ice'
+print(i in s)
+a ='Antidisestablishmentarianism'
+h = 'Honorificabilitudinitatibus'
+print (a>h)
+print(min('Berlioz', 'Borodin', 'Brian', 'Bartok', 'Bellini', 'Buxtehude', 'Bernstein'))
+print(max('Berlioz', 'Borodin', 'Brian', 'Bartok', 'Bellini', 'Buxtehude', 'Bernstein'))
